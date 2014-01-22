212a2963559ab6cd68748b869c6a95dfe3c4cc8b
edire <az8641683@163.com>
2014-01-23 02:17:56 +0800

    f

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *程序官方支持社区 http://youbbs.sinaapp.com/
+ *欢迎交流！
+ *youBBS是开源项目，可自由修改，但要保留Powered by 链接信息
+ */
+
+//数据库主机名或IP
+$servername = 'localhost';
+//数据库用户名
+$dbusername = 'root';
+//数据库密码
+$dbpassword = '8641683';
+//数据库名
+$dbname = 'gist';
+//数据端口
+$dbport = '3306';
+
+//MySQL字符集
+$dbcharset = 'utf8';
+//系统默认字符集
+$charset = 'utf-8';
+
+?>
