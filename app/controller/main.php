89aa46a80cc1451f28609bd7c2843c3169959cbe
edire <az8641683@163.com>
2014-01-25 03:34:18 +0800

    aa

--- app/controller/main.php
+++ app/controller/main.php
@@ -16,9 +16,9 @@ class main extends spController
 		$i=0;
 		foreach($article_sql as $arc){
 			$result[$i][title]=$arc[title];
-			$user_sql=$user->findAll(array('id'=$arc[uid]));
+			$user_sql=$user->findAll(array('id'=>$arc[uid]));
 			$result[$i][user]=$user_sql[0][name];//id
-			$cate_sql=$cate->findAll(array('id'=$arc[cid]));
+			$cate_sql=$cate->findAll(array('id'=>$arc[cid]));
 			$result[$i][cate]=$cate_sql[0][name];//cate
 			$result[$i][addtime]=$arc[addtime];
 			$result[$i][comments]=$arc[comments];
