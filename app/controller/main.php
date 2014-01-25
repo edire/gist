8e89ae52df911363ecc5a5bda93da212add32e9a
edire <az8641683@163.com>
2014-01-25 15:41:19 +0800

    aaa

--- app/controller/main.php
+++ app/controller/main.php
@@ -11,7 +11,39 @@ class main extends spController
      *  @access public
      *
      *************************************************************/
-    function JSON($array) {
+    
+     
+     
+     
+    
+	$article=$this->select();
+    echo $this->JSON($article);
+	}
+	
+	public function select(){
+		$user=spClass('users');
+		$cate=spClass('categories');
+		$article=spClass('articles');
+		
+		
+		$article_sql=$article->findAll();
+		
+		$i=0;
+		foreach($article_sql as $arc){
+			$result[$i][title]=$arc[title];
+			$user_sql=$user->findAll(array('id'=>$arc[uid]));
+			$result[$i][user]=$user_sql[0][name];//id
+			$cate_sql=$cate->findAll(array('id'=>$arc[cid]));
+			$result[$i][cate]=$cate_sql[0][name];//cate
+			$result[$i][addtime]=$arc[addtime];
+			$result[$i][comments]=$arc[comments];
+			$i++;
+		}
+		return $result;
+		
+	}
+	
+	public function JSON($array) {
 			
 			  /**************************************************************
 			 *
@@ -52,34 +84,4 @@ class main extends spController
         $json = json_encode($array);
         return urldecode($json);
     }
-     
-     
-     
-    
-	$article=$this->select();
-    echo JSON($article);
-	}
-	
-	public function select(){
-		$user=spClass('users');
-		$cate=spClass('categories');
-		$article=spClass('articles');
-		
-		
-		$article_sql=$article->findAll();
-		
-		$i=0;
-		foreach($article_sql as $arc){
-			$result[$i][title]=$arc[title];
-			$user_sql=$user->findAll(array('id'=>$arc[uid]));
-			$result[$i][user]=$user_sql[0][name];//id
-			$cate_sql=$cate->findAll(array('id'=>$arc[cid]));
-			$result[$i][cate]=$cate_sql[0][name];//cate
-			$result[$i][addtime]=$arc[addtime];
-			$result[$i][comments]=$arc[comments];
-			$i++;
-		}
-		return $result;
-		
-	}
 }
