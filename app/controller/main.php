11273c2a89719143fd5721c1dc8b97cef4d5f4e6
edire <az8641683@163.com>
2014-01-26 02:03:47 +0800

    aaa

--- app/controller/main.php
+++ app/controller/main.php
@@ -17,7 +17,7 @@ class main extends spController
 	
 	$article=$this->selectlist($conditions);
 			$arr=$this->JSON($article);
-			$json = str_replace('\n','<br>' ,$arr); 
+			$json = preg_replace("'([\r\n])[\s]+'", "<br>", $str);
 			echo $json;
 	}
 	
@@ -31,7 +31,7 @@ class main extends spController
 			
 			$article=$this->selectarc($conditions);
 			$arr=$this->JSON($article);
-			$json = str_replace('\n','<br>',$arr); 
+			$json = preg_replace("'([\r\n])[\s]+'", "<br>", $str);
 			echo $json;
 			
 	}						
