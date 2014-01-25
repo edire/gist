449c7d7b0004ec0d511d66e275af1e297d361b81
edire <az8641683@163.com>
2014-01-25 15:08:53 +0800

    aaa

--- app/controller/main.php
+++ app/controller/main.php
@@ -2,7 +2,34 @@
 class main extends spController
 {
 	function index(){
-    /**************************************************************
+   
+		$article=$this->select();
+		echo $this->JSON($article);
+	}
+	public function select(){
+		$user=spClass('users');
+		$cate=spClass('categories');
+		$article=spClass('articles');
+		
+		
+		$article_sql=$article->findAll();
+		
+		$i=0;
+		foreach($article_sql as $arc){
+			$result[$i][title]=$arc[title];
+			$user_sql=$user->findAll(array('id'=>$arc[uid]));
+			$result[$i][user]=$user_sql[0][name];//id
+			$cate_sql=$cate->findAll(array('id'=>$arc[cid]));
+			$result[$i][cate]=$cate_sql[0][name];//cate
+			$result[$i][addtime]=$arc[addtime];
+			$result[$i][comments]=$arc[comments];
+			$i++;
+		}
+		return $result;
+		
+	}
+	
+	 /**************************************************************
      *
      *  使用特定function对数组中所有元素做处理
      *  @param  string  &$array     要处理的字符串
@@ -44,37 +71,9 @@ class main extends spController
      *
      *************************************************************/
     function JSON($array) {
-        arrayRecursive($array, 'urlencode', true);
+        $this->arrayRecursive($array, 'urlencode', true);
         $json = json_encode($array);
         return urldecode($json);
     }
      
-     
-     
-    
-		$article=$this->select();
-    echo JSON($article);
-	}
-	public function select(){
-		$user=spClass('users');
-		$cate=spClass('categories');
-		$article=spClass('articles');
-		
-		
-		$article_sql=$article->findAll();
-		
-		$i=0;
-		foreach($article_sql as $arc){
-			$result[$i][title]=$arc[title];
-			$user_sql=$user->findAll(array('id'=>$arc[uid]));
-			$result[$i][user]=$user_sql[0][name];//id
-			$cate_sql=$cate->findAll(array('id'=>$arc[cid]));
-			$result[$i][cate]=$cate_sql[0][name];//cate
-			$result[$i][addtime]=$arc[addtime];
-			$result[$i][comments]=$arc[comments];
-			$i++;
-		}
-		return $result;
-		
-	}
 }
