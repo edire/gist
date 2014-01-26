187d0df54b3550506ba6b57d7648c983f2652a8a
edire <az8641683@163.com>
2014-01-26 14:49:17 +0800

    aaa

--- app/controller/main.php
+++ app/controller/main.php
@@ -192,7 +192,7 @@ public function geshihua($str){
 	                  chr(163),
 	                  chr(169),
 	                  "",
-					  "<br>",
+					  "",
 	                  "\\1",
 	                  "chr(\\1)");
 	
