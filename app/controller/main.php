b5acdc693033e44edea0f58554d2bbedca2a1336
edire <az8641683@163.com>
2014-01-27 16:47:10 +0800

    aaa

--- app/controller/main.php
+++ app/controller/main.php
@@ -185,7 +185,7 @@ public function geshihua($str){
 
 
 	$replace = array ("",
-	                  "\\\\",
+	                  "\\\\\\",
 	                  "\"",
 	                  "&",
 	                  "<",
